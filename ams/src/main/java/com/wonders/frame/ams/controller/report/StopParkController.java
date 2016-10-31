8d71dc22cdbd831942758dda2eb0d95b54d63297
CyranoChen <cyrano@arsenalcn.com>
2016-10-31 14:41:49 +0800

    Update ams source code and add lib

--- ams/src/main/java/com/wonders/frame/ams/controller/report/StopParkController.java
+++ ams/src/main/java/com/wonders/frame/ams/controller/report/StopParkController.java
@@ -51,35 +51,42 @@ public class StopParkController {
 		String startTime = session.getAttribute("sstartTime").toString();
 		String endTime = session.getAttribute("sendTime").toString();
 		String time = startTime.equals(endTime) ? startTime : startTime + "到" + endTime;
-		String sqlWhere1=" AND T.AIRLINE_HOME_COUNTRY = 'CN' "
-				+ " AND (T.FLIGHT_PROPERTY = 'B/J'"
-				+ " OR T.FLIGHT_PROPERTY = 'B/W'"
-				+ " OR T.FLIGHT_PROPERTY = 'C/B'"
-				+ " OR T.FLIGHT_PROPERTY = 'E/T'"
-				+ " OR T.FLIGHT_PROPERTY = 'F/H'"
-				+ " OR T.FLIGHT_PROPERTY = 'J/B'"
-				+ " OR T.FLIGHT_PROPERTY = 'L/W'"
-				+ " OR T.FLIGHT_PROPERTY = 'W/Z'"
-				+ " OR T.FLIGHT_PROPERTY = 'Z/P'"
-				+ " OR T.FLIGHT_PROPERTY = 'N/M'"
-				+ " OR T.FLIGHT_PROPERTY = 'Z/X')";
+		String sqlWhere1=" AND T.AIRLINE_HOME_COUNTRY = 'CN' ";
+
+//		String sqlWhere1=" AND T.AIRLINE_HOME_COUNTRY = 'CN' "
+//				+ " AND (T.FLIGHT_PROPERTY = 'B/J'"
+//				+ " OR T.FLIGHT_PROPERTY = 'B/W'"
+//				+ " OR T.FLIGHT_PROPERTY = 'C/B'"
+//				+ " OR T.FLIGHT_PROPERTY = 'E/T'"
+//				+ " OR T.FLIGHT_PROPERTY = 'F/H'"
+//				+ " OR T.FLIGHT_PROPERTY = 'J/B'"
+//				+ " OR T.FLIGHT_PROPERTY = 'L/W'"
+//				+ " OR T.FLIGHT_PROPERTY = 'W/Z'"
+//				+ " OR T.FLIGHT_PROPERTY = 'Z/P'"
+//				+ " OR T.FLIGHT_PROPERTY = 'N/M'" q
+//				+ " OR T.FLIGHT_PROPERTY = 'Z/X')";
 		List<Map<String,Object>> l1 = s.query(startTime,endTime,sqlWhere1);
 		l.add(l1);
-		String sqlWhere2=" AND (T.AIRLINE_HOME_COUNTRY IS NULL OR T.AIRLINE_HOME_COUNTRY<> 'CN') ";
+//		String sqlWhere2=" AND (T.AIRLINE_HOME_COUNTRY IS NULL OR T.AIRLINE_HOME_COUNTRY<> 'CN') ";
+		String sqlWhere2=" AND ( T.AIRLINE_HOME_COUNTRY NOT IN ('CN','GW')  OR T.AIRLINE_HOME_COUNTRY IS NULL ) ";
+
 		List<Map<String,Object>> l2 = s.query(startTime,endTime,sqlWhere2);
 		l.add(l2);
-		String sqlWhere3=" AND T.AIRLINE_HOME_COUNTRY = 'CN' "
-				+ " AND (T.FLIGHT_PROPERTY <> 'B/J'"
-				+ " AND T.FLIGHT_PROPERTY <> 'B/W'"
-				+ " AND T.FLIGHT_PROPERTY <> 'C/B'"
-				+ " AND T.FLIGHT_PROPERTY <> 'E/T'"
-				+ " AND T.FLIGHT_PROPERTY <> 'F/H'"
-				+ " AND T.FLIGHT_PROPERTY <> 'J/B'"
-				+ " AND T.FLIGHT_PROPERTY <> 'L/W'"
-				+ " AND T.FLIGHT_PROPERTY <> 'W/Z'"
-				+ " AND T.FLIGHT_PROPERTY <> 'Z/P'"
-				+ " AND T.FLIGHT_PROPERTY <> 'N/M'"
-				+ " AND T.FLIGHT_PROPERTY <> 'Z/X')";
+
+		String sqlWhere3=" AND T.AIRLINE_HOME_COUNTRY = 'GW' ";
+
+//		String sqlWhere3=" AND T.AIRLINE_HOME_COUNTRY = 'CN' "
+//				+ " AND (T.FLIGHT_PROPERTY <> 'B/J'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'B/W'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'C/B'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'E/T'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'F/H'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'J/B'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'L/W'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'W/Z'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'Z/P'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'N/M'"
+//				+ " AND T.FLIGHT_PROPERTY <> 'Z/X')";
 		List<Map<String,Object>> l3 = s.query(startTime,endTime,sqlWhere3);
 		l.add(l3);
 		es.exportExcel(response, l, "stopPark", time);
--- ams/src/main/java/com/wonders/frame/ams/service/report/DemandService.java
+++ ams/src/main/java/com/wonders/frame/ams/service/report/DemandService.java
@@ -17,7 +17,7 @@ public class DemandService {
     public List<Map<String,Object>> query(String startTime,String endTime,String sqlWhere){
     	String sql="select * from v_demand t where 1 = 1 " + sqlWhere;
     	if((startTime!=null&&!("").equals(startTime))&&(endTime!=null&&!("").equals(endTime))){
-			sql = sql + " and to_date(t.scheduled_time,'yyyy-MM-dd') between to_date(?,'yyyy-MM-dd') and to_date(?,'yyyy-MM-dd') order by t.scheduled_time";
+			sql = sql + " and to_date(t.scheduled_time,'yyyy-MM-dd') between to_date(?,'yyyy-MM-dd') and to_date(?,'yyyy-MM-dd') order by t.SCHEDULED_TIME,t.actual_time,t.LANDING_TIME ";
 		}
     	List<Map<String,Object>> list = jt.queryForList(sql,startTime,endTime);
     	return list;
